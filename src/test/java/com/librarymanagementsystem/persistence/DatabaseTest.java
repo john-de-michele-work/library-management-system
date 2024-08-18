6f1956b8675ce46a45aa0d923f66b7310a279a20
John de Michele <vnd_john.demichele@gennev.com>
2024-08-17 20:48:27 -0700

    Created test to verify schema is actually being created.

---
+++ src/test/java/com/librarymanagementsystem/persistence/DatabaseTest.java
@@ -0,0 +1,37 @@
+package com.librarymanagementsystem.persistence;
+
+import com.librarymanagementsystem.data.Book;
+import org.hibernate.Session;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * A test to verify that the schema is being created
+ */
+public final class DatabaseTest {
+
+    private Session session;
+
+    @BeforeEach
+    public void setup() {
+        SessionManager sessionManager = new SessionManager();
+        sessionManager.startSession();
+        session = sessionManager.getSession();
+    }
+
+    @Test
+    public void queryTest() {
+        List<Book> books = session.createQuery("from Book", Book.class).getResultList();
+        assertEquals(0, books.size(), "Books were created from nothing!");
+    }
+
+    @AfterEach
+    public void teardown() {
+        session.close();
+    }
+}
